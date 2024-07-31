8bfa208950b08b4bd780576ec76fc9290dd3ad18
Unashe Siwawa <anelesiwawa@gmail.com>
2024-07-31 13:04:53 +0200

    Updates
    -- checking deployment

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,3 +1,2 @@
 export const DATABASE_CONNECTION = 'DATABASE_CONNECTION';
 export const JWT_SECRET = 'UnasheIsGoingToStartAYouthChurchCEYC';
-export const MONGOOSE_CONNECTION_STRING = 'mongodb://farmerztoolmongodv:rq2pBsQNW3TVJKn9JIwtzXT2riXlC180BdEkvaP9GCxEmtGpuNrgvQgy3jv9mW7VkynqzC9K49llACDbLof9tw%3D%3D@farmerztoolmongodv.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@farmerztoolmongodv@';
--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -5,6 +5,6 @@ export const databaseProviders = [
   {
     provide: DATABASE_CONNECTION,
     useFactory: async (): Promise<typeof mongoose> =>
-      await mongoose.connect(MONGOOSE_CONNECTION_STRING),
+      await mongoose.connect(process.env.MONGOOSE_CONNECTION_STRING),
   },
 ];
