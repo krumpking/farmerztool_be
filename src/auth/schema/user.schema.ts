7ae9eadf70d859f473b10ef34886ee80adf1de87
Ronnie Kakunguwo <ronniekakunguwo03@gmail.com>
2024-09-04 05:36:39 +0200

    "Added user verification feature with OTP, updated auth controller and service, and modified user schema to include verified field and unique email constraint"

--- src/auth/schema/user.schema.ts
+++ src/auth/schema/user.schema.ts
@@ -7,9 +7,10 @@ export const UserSchema = new mongoose.Schema({
   dateJoined: {type: String, required: false},
   role: {type: String, enum: ["Admin", "Manager", "Finance", "Animal Manger", "Crop Management"], required: false},
   permissions: {type: [String], default: []},
-  email: {type: String  , required: true},
+  email: {type: String  , unique: true, required: true},
   password: {type: String, required: true},
   farmArea: {type: String, required: false},
+  verified: {type: Boolean, default: false},
   otp: {type: String, required: false},
   otpCreatedAt: {type: Date, required: false}
 });
