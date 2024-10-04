15197d325b87491e5bf4b3a92941442c4b3cadfd
Ronnie Kakunguwo <ronniekakunguwo03@gmail.com>
2024-10-04 16:19:56 +0200

    enabled cors

--- src/hatchery/hatchery.controller.ts
+++ src/hatchery/hatchery.controller.ts
@@ -232,7 +232,7 @@ export class HatcheryController {
   @Permissions(Permission.Read)
   @ApiOperation({
     summary: "Get egg performance",
-    description: "Get egg performance. NB: Please note that here are using query params so we have to do this in the url using postman url= .../kpi/customer-performance?animalType=Chicken&startEnd=2022-01-01&endDate=2024-01-01. NB: date format is %Y-%m-%d",
+    description: "Get egg performance. NB: Please note that here are using query params so we have to do this in the url using postman url= .../kpi/customer-performance?animalType=Chicken&startEnd=2022-01-01&endDate=2024-01-01. NB: date format is %Y-%m-%d OR Use Query Params in Postman",
     responses: {
       200: {
         description: 'Egg performance fetched successfully',
