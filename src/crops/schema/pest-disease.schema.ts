77e6f9737e61e9f5d190559942e835f3f0248962
Unashe Siwawa <anelesiwawa@gmail.com>
2024-10-11 22:18:22 +0200

    Fixes
    --Fixes

--- src/crops/schema/pest-disease.schema.ts
+++ src/crops/schema/pest-disease.schema.ts
@@ -2,7 +2,7 @@ import {Schema } from 'mongoose';
 
 
 export const pestDiseaseIssueSchema: Schema = new Schema({
-  cropId: {
+  crop: {
     type: Schema.Types.ObjectId,
     ref: 'Crop',
     required: true
