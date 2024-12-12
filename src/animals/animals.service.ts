35b708a330b3e1b0327a32849ebd446ca144b25a
Ronnie Kakunguwo <ronniekakunguwo03@gmail.com>
2024-12-12 23:17:37 +0200

    `Fixed condition in animal creation logic`

--- src/animals/animals.service.ts
+++ src/animals/animals.service.ts
@@ -77,13 +77,13 @@ export class AnimalsService {
         addedByType: user.userType
       });
 
-      const createdAnimal = await this.animalModel.findById(newAnimalInstance._id);
 
-      if (!createdAnimal) {
+
+      if (!newAnimalInstance) {
         return ResponseHandler.handleBadRequest("Failed to create animal record");
       }
 
-      return ResponseHandler.handleCreated("Animal record created successfully", createdAnimal);
+      return ResponseHandler.handleCreated("Animal record created successfully", newAnimalInstance);
 
     } catch (error) {
       console.log(error);
